2ea4b9ba78bc018d06e3a5b9db8957995e9e92f4
yue1123 <2572468699@qq.com>
2023-05-10 01:22:48 +0800

    chore: blob 调研

--- example/src/components/HelloWorld.vue
+++ example/src/components/HelloWorld.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
   import { ref } from 'vue'
   import base64Img from '../assets/44051676621973_.pic.jpg?base64'
-  
+
   defineProps<{ msg: string }>()
 
   const count = ref(0)
+  function base64ToBlob(base64String: string) {
+    const mimeMatch = base64String.match(/^data:(.*);base64,/)
+    const contentType = mimeMatch ? mimeMatch[1] : ''
+    console.log(contentType)
+    const bytes = new Uint8Array(base64String.length)
+    for (let i = 0; i < base64String.length; i++) {
+      bytes[i] = base64String.charCodeAt(i)
+    }
+    // const decoder = new TextDecoder('utf-8')
+    // const decodedData = decoder.decode(bytes)
+    // const mimeMatch = decodedData.match(/^data:(.*);base64,/)
+    // const contentType = mimeMatch ? mimeMatch[1] : ''
+    // const blob = new Blob([bytes], { type: 'text/plain' })
+    return blob
+  }
+  function blobToUrl(blob) {
+    const url = URL.createObjectURL(blob)
+    return url
+  }
+  const blobUrl = blobToUrl(base64ToBlob(base64Img))
+  console.log(blobUrl)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <img :src="base64Img" alt="" />
+  <img :src="blobUrl" alt="" />
   <hr />
   <textarea disabled rows="10" :cols="40">
     {{ base64Img }}
