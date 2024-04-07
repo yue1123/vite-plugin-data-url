cc4c5d05bb74d29599a14a3a3b6d78fcc5088f6b
yue1123 <2572468699@qq.com>
2024-04-07 19:31:27 +0800

    feat: remove example size limit

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,11 +4,5 @@ import { dataUrlQuery } from 'vite-plugin-data-url-query'
 import inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    dataUrlQuery({
-      limit: 100
-    }),
-    inspect()
-  ]
+  plugins: [vue(), dataUrlQuery(), inspect()]
 })
